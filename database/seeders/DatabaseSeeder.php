7904ca1b1609f346febe44bce129b5a54c1ba283
Elites2017 <daveluv09@gmail.com>
2025-08-07 14:24:59 +0200

    add categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,30 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // create a user for the user table in case the table is empty
+        User::factory()->create([
+            'name' => 'Jane Doe',
+            'email' => 'jane@medium.com'
+        ]);
+
+        // define some categories for the database
+        $categories = [
+            'Tehnology',
+            'Health',
+            'Science',
+            'Sports',
+            'Politics',
+            'Entertainment'
+        ];
+
+        // loop over the categories array to create each one
+        foreach ($categories as $key => $category) {
+            # code...
+            Category::create(['name' => $category]);
+        }
+
+        // create 100 posts
+        Post::factory(100)->create();
     }
 }

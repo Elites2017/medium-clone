efb3129ed4ef5570324c1d424b567d49ac08a785
Elites2017 <daveluv09@gmail.com>
2025-08-09 04:04:17 +0200

    add category tabs with slot

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-</div>
+<ul class="flex flex-wrap text font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
+
+    <li class="me-2">
+        <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
+            aria-current="page">All</a>
+    </li>
+
+    @forelse ($categories as $category)
+        <li class="me-2">
+            <a href="#"
+                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
+                {{ $category->name }}
+            </a>
+        </li>
+    @empty
+        {{-- $slot, what's going to be inside the component where it's called  --}}
+        {{ $slot }}
+    @endforelse
+
+</ul>

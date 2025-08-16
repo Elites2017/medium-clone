fea60a6b4c0d817841c6dad7caa2f3af36959716
Elites2017 <daveluv09@gmail.com>
2025-08-16 19:24:45 +0200

    make the created by text strong

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -16,7 +16,8 @@
         </div>
 
         <div class="flex items-center gap-2 text-gray-400 text-small mb-2">
-            {{ $post->getCreatedAt() }}
+            By: <a href="{{ route('profile.show', $post->user) }}"><strong>{{ $post->user->name }}</strong></a>
+            on: <strong>{{ $post->getCreatedAt() }}</strong>
             <span class="inline-flex items-center gap-1">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5"
                         stroke="currentColor" class="size-6">

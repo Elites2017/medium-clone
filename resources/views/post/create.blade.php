e26bac1575d11796bd17d911b4d6d1a886c5c1b1
Elites2017 <daveluv09@gmail.com>
2025-08-16 19:26:26 +0200

    add published at field for post create

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <h1 class="text-3xl mb-4">Create New Post</h1>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
-
                 <form action="{{ route('post.store') }}" class="form-group" enctype="multipart/form-data" method="post">
                     @csrf
                     <!-- Image -->
@@ -39,6 +39,14 @@
                         <x-text-area-input id="content" class="block mt-1 w-full" name="content"
                             :value="old('content')" required />
                         <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
+
+                    <!-- Published At -->
+                    <div class="mt-4">
+                        <x-input-label for="published_at" :value="__('Published At')" />
+                        <x-text-input id="published_at" class="block mt-1 w-full" type="datetime-local" name="published_at"
+                            :value="old('published_at')" required autofocus />
+                        <x-input-error :messages="$errors->get('published_at')" class="mt-2" />
                     </div>
 
                     <div class="group mt-4">
